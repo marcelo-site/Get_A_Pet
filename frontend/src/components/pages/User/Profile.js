ba1cf0f0b92f0cd7ec195b3d2e499f6f9570c87f
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-07-04 19:38:09 -0300

    dotenv

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -9,10 +9,11 @@ import Input from '../../form/Input'
 import useFlashMessage from '../../../hooks/useFlashMessage'
 
 function Profile() {
+    console.log(process.env)
+    const [preview, setPreview] = useState('')
     const {setFlashMessage} = useFlashMessage()
     const [user, setUser] = useState({})
     const [token] = useState(localStorage.getItem('token') || '')
-    console.log(user) 
     useEffect(() => {
         api.get('/users/checkuser', {
             headers: {
@@ -21,7 +22,7 @@ function Profile() {
         }).then(res => setUser(res.data))
     }, [token])
     function onFileChange (e) {
-        
+        setPreview(e.target.files[0])
         setUser({...user, [e.target.name]: e.target.files[0]})
     }
     function handleChange (e) {
@@ -51,16 +52,16 @@ function Profile() {
             msgType= 'error'
             return error.response.data
         })
-
         setFlashMessage(data.message, msgType)
-
     }
     return (
         <section>
             <h1>Profile</h1>
                 <div className={styles.profile_header}> 
                     <h1>Perfil</h1>
-                    <p>Preview image</p>
+                    {(user.image || preview) && (
+                        <img src={preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}img/users/${user.image}`} />
+                    )}
                 </div>
             <form onSubmit={handleSubmit} className={formstyles.form_container}>
                 <Input
